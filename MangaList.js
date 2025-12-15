02824194b276d2e0810fbc1604b2c4c457fef08e
Danny79k <belloli.danny@gmail.com>
2025-12-15 18:47:19 +0000

    trabajado en el backend

--- MangaList.js
+++ MangaList.js
@@ -1,3 +1,6 @@
+const inputSearchManga = document.querySelector('.input-search-manga');
+const mangaContainer = document.querySelector('.manga-grid');
+
 async function getMangas(q) {
     try {
         const response = await fetch(`http://localhost:3000/mangaList?q=${encodeURIComponent(q)}`, {
@@ -8,10 +11,23 @@ async function getMangas(q) {
         }
         const data = await response.json();
         console.log(data);
+        for (const manga of data.data) {
+            const mangaCard = document.createElement('div');
+            mangaCard.classList.add('manga-card');
+            mangaCard.innerHTML = `
+            <h3>${manga.node.title}</h3>
+            <img src="${manga.node.main_picture.medium}" alt="${manga.node.title}">`;
+            mangaContainer.appendChild(mangaCard);
+        }
     }
     catch (error) {
         console.error(`fetch error: ${error}`)
     }
 }
 
-getMangas("naruto");
+inputSearchManga.addEventListener('input', (e) => {
+    const searchQuery = e.target.value || '';
+    getMangas(searchQuery);
+    mangaContainer.innerHTML = '';
+
+})
